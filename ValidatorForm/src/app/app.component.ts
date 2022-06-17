7561f3441bb3d09dba55f8038042316c21a709b3
Swetha'sRepository <59050311+SwethasRepository@users.noreply.github.com>
2022-06-17 16:53:07 +0530

    Add files via upload

---
+++ ValidatorForm/src/app/app.component.ts
@@ -0,0 +1,28 @@
+
+import { Component } from '@angular/core';
+import { FormGroup, FormControl, AbstractControl, ValidationErrors } from '@angular/forms'
+import { gte } from './gte.validator';
+ 
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+ 
+  constructor() {
+  }
+ 
+  myForm = new FormGroup({
+    numVal: new FormControl('', [gte]),
+  })
+ 
+  get numVal() {
+    return this.myForm.get('numVal');
+  }
+ 
+  onSubmit() {
+    console.log(this.myForm.value);
+  }
+}
+ 
